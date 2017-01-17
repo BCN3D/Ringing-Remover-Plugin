827f849d35979fd46ed4e370f1df0237e2f0cf0b
Guillem <guillemavila@gmail.com>
2017-01-17 12:20:32 +0100

    Plugin made universal

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Ringing Remover Plugin for Cura
+
 Plugin to remove the ringing artifact on corners. Specifically designed for the BCN3D Sigma.
 
-To use it Copy&Paste the file **'RingingRemover.py'** into Cura Plugins folder.
+## Requirements
+
+- Cura slicing software.
+- Printer runing the BCN3D Sigma firmware, Marlin, Sprinter, Smoothie, MK4duo.
+
+## Usage
+
+Copy&Paste the file **'RingingRemover.py'** into Cura Plugins folder.
 
 You can find it in Cura (Full settings) > Plugins tab > Open plugin location.
 
--- RingingRemover.py
+++ RingingRemover.py
@@ -1,11 +1,17 @@
 #Name: Ringing Remover
-#Info: Change accelerations to avoid the 'ringing' surface artifact
+#Info: Change accelerations to avoid the 'ringing' surface artifact. Use a default value of 2000 for the BCN3D Sigma.
 #Depend: GCode
 #Type: postprocess
+#Param: defaultAcceleration(int:2000) Default machine acceleration (mm/sec^2)
 
 __copyright__ = "Copyright (C) 2017 Guillem Avila - Released under terms of the AGPLv3 License"
 
-import re, base64, zlib, sys
+import base64, zlib, sys
+
+try:
+	defaultAcceleration = int(defaultAcceleration)
+except:
+	defaultAcceleration = 2000 # set BCN3D Sigma Value
 
 if sys.version_info[0] < 3:
 	import ConfigParser
@@ -28,7 +34,7 @@ def loadGlobalProfileFromString(options):
 			(key, value) = option.split('=', 1)
 			globalProfileParser.set('alterations', key, value)
 
-def accelerationForPerimeters(nozzleSize, layerHeight, outerWallSpeed, base = 5, multiplier = 30000, defaultAcceleration = 2000):
+def accelerationForPerimeters(nozzleSize, layerHeight, outerWallSpeed, defaultAcceleration, base = 5, multiplier = 30000):
 	return min(defaultAcceleration, int(base * round((nozzleSize * layerHeight * multiplier * 1/(outerWallSpeed**(1/2.)))/float(base))))
 
 with open(filename, "r") as f:
@@ -42,11 +48,10 @@ loadGlobalProfileFromString(line[line.find(':')+1:].strip())
 
 with open(filename, "w") as f:
 	for line in lines:
-		line = line.replace(';TYPE:WALL-INNER', ';TYPE:WALL-INNER\r\nM204 S2000')
-		line = line.replace(';TYPE:WALL-OUTER', ';TYPE:WALL-OUTER\r\nM204 S'+str(accelerationForPerimeters(float(globalProfileParser.get('profile', 'nozzle_size')), float(globalProfileParser.get('profile', 'layer_height')), float(globalProfileParser.get('profile', 'inset0_speed')))))
-		line = line.replace(';TYPE:SKIN', ';TYPE:SKIN\r\nM204 S2000')
-		line = line.replace(';TYPE:FILL', ';TYPE:FILL\r\nM204 S2000')
-		line = line.replace(';TYPE:SUPPORT', ';TYPE:SUPPORT\r\nM204 S2000')
-		line = line.replace(';CURA_PROFILE_STRING', 'M204 S2000\r\n;CURA_PROFILE_STRING')
-		line = line.replace(';CURA_PROFILE_STRING', 'M204 S2000\r\n;CURA_PROFILE_STRING')
+		line = line.replace(';TYPE:WALL-INNER', ';TYPE:WALL-INNER\r\nM204 S'+str(defaultAcceleration))
+		line = line.replace(';TYPE:WALL-OUTER', ';TYPE:WALL-OUTER\r\nM204 S'+str(accelerationForPerimeters(float(globalProfileParser.get('profile', 'nozzle_size')), float(globalProfileParser.get('profile', 'layer_height')), float(globalProfileParser.get('profile', 'inset0_speed')), defaultAcceleration)))
+		line = line.replace(';TYPE:SKIN', ';TYPE:SKIN\r\nM204 S'+str(defaultAcceleration))
+		line = line.replace(';TYPE:FILL', ';TYPE:FILL\r\nM204 S'+str(defaultAcceleration))
+		line = line.replace(';TYPE:SUPPORT', ';TYPE:SUPPORT\r\nM204 S'+str(defaultAcceleration))
+		line = line.replace(';CURA_PROFILE_STRING', 'M204 S'+str(defaultAcceleration)+'\r\n;CURA_PROFILE_STRING')
 		f.write(line)
